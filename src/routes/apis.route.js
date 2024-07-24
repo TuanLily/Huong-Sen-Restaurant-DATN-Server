d4cb3bd971e7e81cb2b41686149b227498312539
TuanLily <122657799+TuanLily@users.noreply.github.com>
2024-07-24 13:59:31 +0700

    [HS-25] - Hoàn thành tạo customers API
    [HS-25] - Hoàn thành tạo customers API

--- src/routes/apis.route.js
+++ src/routes/apis.route.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+
+
 const productCategoriessApi = require('../apis/product_categories.api');
+const CustomerApi = require('../apis/customers.api');
 
 
 router.use('/product_categories', productCategoriessApi);
+router.use('/customer', CustomerApi);
 
 
 module.exports = router;
